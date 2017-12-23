6a346c405e7d27874460b910c73d838da9b6f912
Boris <kubiakboris@gmail.com>
2017-12-23 23:57:50 +0100

    Create a nice UI

--- js/index.js
+++ js/index.js
@@ -7,9 +7,12 @@ let stop = false;
 let lastTick = null;
 let difficulty = 0;
 const step = 250;
-const counter = document.getElementById('counter');
-const speed = document.getElementById('speed');
-const probability = document.getElementById('probability');
+const elements = {
+	counter: document.getElementById('counter'),
+	speed: document.getElementById('speed'),
+	probability: document.getElementById('probability'),
+	probabilityBar: document.getElementById('probability-bar')
+};
 
 const parseInput = () => {
 	return {
@@ -20,22 +23,24 @@ const parseInput = () => {
 
 const incrementCounter = incr => {
 	count += incr;
-	counter.innerText = count.toString() + (count === 1 ? ' address' : ' addresses') + ' generated';
+	elements.counter.innerText = count.toString() + (count === 1 ? ' address' : ' addresses');
 
 	const currentTick = performance.now();
-	speed.innerText = Math.floor(1000 * incr / (currentTick - lastTick)) + ' addresses / second';
+	elements.speed.innerText = Math.floor(1000 * incr / (currentTick - lastTick)) + ' addresses / second';
 	lastTick = currentTick;
 };
 
 const updateStats = () => {
 	const prob = Math.round(10000 * vanity.computeProbability(difficulty, count)) / 100;
-	probability.innerText = 'Probability: ' + prob + '%';
+	elements.probability.innerText = prob + '%';
+	elements.probabilityBar.style.width = prob + '%';
 };
 
 const displayResult = result => {
 	incrementCounter(result ? result.attempts : 0);
 	updateStats();
-	document.getElementById('result').innerHTML = result ? 'address: ' + result.address + '<br>key: ' + result.privKey : '';
+	document.getElementById('address').innerText = result ? result.address : '';
+	document.getElementById('private-key').innerText = result ? result.privKey : '';
 };
 
 const generate = input => {
@@ -63,7 +68,7 @@ document.getElementById('gen').addEventListener('click', () => {
 
 	const input = parseInput();
 	difficulty = vanity.computeDifficulty(input.pattern, input.checksum);
-	document.getElementById('difficulty').innerText = 'difficulty: ' + difficulty;
+	document.getElementById('difficulty').innerText = difficulty.toString();
 	generate(input);
 });
 
