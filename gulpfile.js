631f247daca0b368dc86a06c81032629459ad02a
Boris Kubiak <kubiakboris@gmail.com>
2018-01-11 08:35:53 +0100

    Improve build task

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ const source = require('vinyl-source-stream');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 
 // Browserify
@@ -35,8 +34,7 @@ gulp.task('build-js', ['browserify'], cb => {
 		gulp.src(['js/index.js', 'js/bundle.js']),
 		babel({presets: ['env'], plugins: ['@babel/plugin-transform-object-assign']}),
 		uglify(),
-		rename({extname: '.min.js'}),
-		gulp.dest('build/js')
+		gulp.dest('js')
 	], cb);
 });
 
@@ -50,25 +48,19 @@ gulp.task('build-css', ['sass'], cb => {
 			cascade: false
 		}),
 		cleanCSS({compatibility: 'ie8'}),
-		rename({extname: '.min.css'}),
-		gulp.dest('build/css')
-	], cb);
-});
-
-gulp.task('replace-path', cb => {
-	pump([
-		gulp.src('index.html'),
-		replace('css/stylesheet.css', 'build/css/stylesheet.min.css'),
-		replace('js/index.js', 'build/js/index.min.js'),
-		gulp.dest('./'),
-		gulp.src('js/index.js'),
-		replace('js/bundle.js', 'build/js/bundle.min.js'),
-		gulp.dest('js')
+		gulp.dest('css')
 	], cb);
 });
 
 // Build app
-gulp.task('build', ['replace-path', 'build-js', 'build-css']);
+gulp.task('build', ['build-js', 'build-css'], cb => {
+	pump([
+		gulp.src('.gitignore'),
+		replace('js/bundle.js', ''),
+		replace('css/stylesheet.css', ''),
+		gulp.dest('./')
+	], cb);
+});
 
 // Watch changes and compile on the fly
 gulp.task('watch', () => {
