aec85c7c6813c34ed044258e34a25ea6848e0d75
Boris Kubiak <kubiakboris@gmail.com>
2018-01-27 13:38:00 +0100

    Lint files

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,79 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: './src/main.js'
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    extractCSS: true,
-                }
-            },
-            {
-                test: /vanity\.js$/,
-                loader: 'worker-loader',
-                exclude: /node_modules/,
-                options: {
-                    inline: true,
-                    name: '[name].[ext]?[hash]'
-                }
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|woff2)/,
-                exclude: /node_modules/,
-                loader: 'url-loader',
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin("style.css")
-    ],
-    resolve: {
-        alias: {
-            vue$: 'vue/dist/vue.esm.js'
-        },
-        extensions: ['*', '.js', '.vue', '.json']
-    },
-    devServer: {
-        historyApiFallback: true,
-        noInfo: true,
-        overlay: true
-    },
-    performance: {
-        hints: false
-    },
+	entry: {
+		index: './src/main.js'
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/dist/',
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					extractCSS: true
+				}
+			},
+			{
+				test: /vanity\.js$/,
+				loader: 'worker-loader',
+				exclude: /node_modules/,
+				options: {
+					inline: true,
+					name: '[name].[ext]?[hash]'
+				}
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(png|woff2)/,
+				exclude: /node_modules/,
+				loader: 'url-loader'
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('style.css')
+	],
+	resolve: {
+		alias: {
+			vue$: 'vue/dist/vue.esm.js'
+		},
+		extensions: ['*', '.js', '.vue', '.json']
+	},
+	devServer: {
+		historyApiFallback: true,
+		noInfo: true,
+		overlay: true
+	},
+	performance: {
+		hints: false
+	}
 };
 
 if (process.env.NODE_ENV === 'production') {
     // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {NODE_ENV: '"production"'}
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        })
-    ]);
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {NODE_ENV: '"production"'}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: false,
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		})
+	]);
 }
