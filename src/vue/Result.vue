1acaa6c64bcbd5cfc50f95365d820809ad43e0a8
Boris Kubiak <kubiakboris@gmail.com>
2018-02-09 15:18:16 +0100

    Display address identicon in result

--- src/vue/Result.vue
+++ src/vue/Result.vue
@@ -1,21 +1,42 @@
 <template>
     <div class="panel result">
-        <div>Address: <span class="output" v-text="address"></span></div>
-        <div>Private key: <span class="output" v-text="privateKey"></span></div>
+        <div class="row">
+            <div class="float-left" id="identicon"></div>
+            <div class="col">
+                <div>Address: <span class="output" v-text="address"></span></div>
+                <div>Private key: <span class="output" v-text="privateKey"></span></div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import * as blockies from 'blockies';
+
     export default {
         props: {
             address: String,
             privateKey: String
+        },
+        watch: {
+            address(addr) {
+                const id = document.getElementById('identicon');
+                id.innerHTML= '';
+                if(addr){
+                    id.appendChild(blockies({seed: addr.toLocaleLowerCase(), scale: 6}));
+                }
+            }
         }
     }
 </script>
 
 <style lang="sass" scoped>
     @import "../css/variables"
+    #identicon
+        width: 48px
+        height: 48px
+        margin-right: 15px
+        background-color: $panel-background-clear
     .output
         font-family: monospace
         font-size: 1.2em
