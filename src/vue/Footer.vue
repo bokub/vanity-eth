2fa8c01f6d15f852cff741ff13f76036adcb7ce2
Boris Kubiak <kubiakboris@gmail.com>
2018-02-16 08:46:46 +0100

    Improve footer appearance

--- src/vue/Footer.vue
+++ src/vue/Footer.vue
@@ -2,34 +2,20 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-12 col-lg-6 addr">
+                <div class="col-12 col-lg-6 address">
                     Tips:
-                    <span class="donation-address" v-text="tipsAddress"></span>
+                    <a :href="'https://etherscan.io/address/' + tipsAddress" target="_blank" v-text="tipsAddress"></a>
                 </div>
-                <div class="col-12 col-lg-6">
-                    <div class="row">
-                        <div class="col-6 col-sm-4 no-gutter">
-                            <a :href="'https://etherscan.io/address/' + tipsAddress" target="_blank">
-                                <button type="button">
-                                    <i class="icon-ethereum"></i>&nbsp;&nbsp;&nbsp;Donate
-                                </button>
-                            </a>
-                        </div>
-                        <div class="col-6 col-sm-4 no-gutter">
-                            <a href="https://github.com/bokub/vanity-eth/stargazers" target="_blank">
-                                <button type="button" id="btn-star">
-                                    <i class="icon-star"></i>&nbsp;&nbsp;&nbsp;Star&nbsp;me
-                                </button>
-                            </a>
-                        </div>
-                        <div class="col-4 no-mobile no-gutter">
-                            <a href="https://github.com/bokub/vanity-eth/wiki/download-Vanity-ETH" target="_blank">
-                                <button type="button" id="btn-download">
-                                    <i class="icon-download"></i>&nbsp;&nbsp;&nbsp;Download
-                                </button>
-                            </a>
-                        </div>
-                    </div>
+                <div class="col-12 col-lg-6 links">
+                    <a :href="'https://etherscan.io/address/' + tipsAddress" target="_blank">
+                        <i class="icon-ethereum"></i>&nbsp;&nbsp;&nbsp;Donate
+                    </a>
+                    <a href="https://github.com/bokub/vanity-eth/stargazers" target="_blank">
+                        <i class="icon-star"></i>&nbsp;&nbsp;&nbsp;Star&nbsp;me
+                    </a>
+                    <a href="https://github.com/bokub/vanity-eth/wiki/download-Vanity-ETH" target="_blank">
+                        <i class="icon-download"></i>&nbsp;&nbsp;&nbsp;Download
+                    </a>
                 </div>
             </div>
         </div>
@@ -49,38 +35,24 @@
 <style lang="sass" scoped>
     @import "../css/variables"
     footer
-        padding: 1.5rem 0
+        padding: 1rem 0 0.5rem
         background-color: $panel-background
-        color: $white-text
-        .addr
+        color: $grey-text
+        a
+            text-decoration: none
+        .address
             margin-bottom: 20px
-        .donation-address
-            font-family: monospace
-            font-size: 1.2em
-            color: $grey-text
-            margin-left: 15px
-            word-break: break-all
-        button
-            border: none
-            outline: none
             color: $white-text
-            padding: 0 0.8em
-            line-height: 40px
-            font-weight: 500
-            cursor: pointer
-            -webkit-appearance: none
-            background-color: $teal
-            i
-                font-size: 1.3em
-                position: relative
-                top: 2px
-                color: lighten($teal, 40)
-            &:hover
-                background-color: $yellow
+            a
+                font-family: monospace
+                font-size: 1.2em
+                margin-left: 15px
+                word-break: break-all
+        .links
+            text-align: right
+            a
+                margin-right: 30px
+                padding-bottom: 2px
                 i
-                    color: lighten($yellow, 40)
-
-    @media screen and (max-width: 576px)
-        .no-mobile
-            display: none
+                    font-size: 1.2em
 </style>
