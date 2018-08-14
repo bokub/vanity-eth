0c5d0dfe3bc709325e2aaccfbdbb6f39a502af31
Boris Kubiak <kubiakboris@gmail.com>
2018-08-14 08:43:31 +0200

    Add fathom snippet for testing purposes

--- src/vue/Corner.vue
+++ src/vue/Corner.vue
@@ -32,6 +32,19 @@
                 this.cid = uuidv1();
                 localStorage.setItem('cid', this.cid);
             }
+            // Fathom - simple website analytics - https://github.com/usefathom/fathom
+            (function (f, a, t, h, o, m) {
+                a[h] = a[h] || function () {
+                    (a[h].q = a[h].q || []).push(arguments)
+                };
+                o = f.createElement('script');
+                m = f.getElementsByTagName('script')[0];
+                o.async = 1;
+                o.src = t;
+                o.id = 'fathom-script';
+                m.parentNode.insertBefore(o, m)
+            })(document, window, 'https://stats.vanity-eth.tk/tracker.js', 'fathom');
+            fathom('trackPageview');
         }
     }
 </script>
