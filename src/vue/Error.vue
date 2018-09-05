990a35a31179d937858667ed9da538252728480d
Boris Kubiak <kubiakboris@gmail.com>
2018-09-05 18:59:04 +0200

    fix: Remove typo again; fix link

--- src/vue/Error.vue
+++ src/vue/Error.vue
@@ -8,9 +8,9 @@
         <div v-if="error === 'insecure_location'">
             <h3>Security alert</h3>
 
-            It seems like you are using Vanity-ETH from an unknown website, which could stole your private key.<br>
-            To stay safe, use Vanity-ETH on <a href="https://vanity-eth.tk">vanity-eth.tk</a>, or download the latest
-            build <a href="https://git.io/veth-dl" target="_blank">here</a> to use offline.
+            You are using Vanity-ETH from an unknown website, which could steal your private keys.<br>
+            To stay safe, use Vanity-ETH on <a href="https://vanity-eth.tk" target="_blank">vanity-eth.tk</a>, or
+            download the latest build <a href="https://git.io/veth-dl" target="_blank">here</a> to use offline.
         </div>
         <p v-else v-html="error.replace('\n', '<br>')"></p>
     </div>
