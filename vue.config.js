304de45cdd5c30cfa92d14dc09f0c28ce43a02bc
tabcat <tabcat00@proton.me>
2023-01-23 00:15:03 -0600

    fix: builds without warnings from npm audit
    add node plugin to vue webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const prettier = require('prettier');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
     publicPath: '',
@@ -17,20 +18,23 @@ module.exports = {
             .end();
     },
     configureWebpack: {
-        plugins: process.env.DEPLOY
-            ? [
-                  new (require('prerender-spa-plugin'))({
-                      staticDir: path.join(__dirname, 'dist'),
-                      routes: ['/'],
-                      postProcess(renderedRoute) {
-                          renderedRoute.html = prettier
-                              .format(renderedRoute.html, { filepath: 'index.html', printWidth: 120 })
-                              .replace('render', 'prerender')
-                              .replace(/(data-v-[0-9a-f]+)=""/gm, '$1');
-                          return renderedRoute;
-                      },
-                  }),
-              ]
-            : [],
+        plugins: [
+            new NodePolyfillPlugin(),
+            ...(process.env.DEPLOY
+                ? [
+                      new (require('prerender-spa-plugin'))({
+                          staticDir: path.join(__dirname, 'dist'),
+                          routes: ['/'],
+                          postProcess(renderedRoute) {
+                              renderedRoute.html = prettier
+                                  .format(renderedRoute.html, { filepath: 'index.html', printWidth: 120 })
+                                  .replace('render', 'prerender')
+                                  .replace(/(data-v-[0-9a-f]+)=""/gm, '$1');
+                              return renderedRoute;
+                          },
+                      }),
+                  ]
+                : []),
+        ],
     },
 };
