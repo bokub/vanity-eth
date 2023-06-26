f53a68bc706df741e07cdb78b0e2b4e3999e54dd
bokub <kubiakboris@gmail.com>
2023-06-26 12:41:52 +0000

    Deploying to production from @ bokub/vanity-eth@3f112321fdd782c0ab9f3f8c962b9638ac3cfd41 🚀

---
+++ precache-manifest.349eb0642e88af1e95fe55c8d011e303.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2153922c033bf3fdb4705ebf7be31010",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "d4cdd378ffe7495aab66850f9a3d275c",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "364a7f6c755e2b23b29a7603d737f298",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "502659629cb03dadf338",
+    "url": "css/app.05396ab5.css"
+  },
+  {
+    "revision": "42ba3d241752489317b8",
+    "url": "css/chunk-vendors.502acf74.css"
+  },
+  {
+    "revision": "4cb60baf7ec071b1f2edc5855c4aed58",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "42cbe675b4e2a81d4496527031e63c5c",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "f1a4a058fbba1e35a406188ae7eddaf8",
+    "url": "fonts/lato-regular.f1a4a058.woff2"
+  },
+  {
+    "revision": "79982cd1f74c6fa7451bf9b37ead09ff",
+    "url": "fonts/montserrat-bold.79982cd1.woff2"
+  },
+  {
+    "revision": "501ce09c42716a2f6e1503a25eb174c9",
+    "url": "fonts/montserrat.501ce09c.woff2"
+  },
+  {
+    "revision": "e92cc0fb9e1a7debc138224fd02a462a",
+    "url": "fonts/roboto-mono.e92cc0fb.woff2"
+  },
+  {
+    "revision": "4c36a510ecb00d065d837ba56471ebe4",
+    "url": "img/tick-mark.4c36a510.png"
+  },
+  {
+    "revision": "8f7fdb15977d751bd0703916077905e4",
+    "url": "index.html"
+  },
+  {
+    "revision": "502659629cb03dadf338",
+    "url": "js/app.cfab15d4.js"
+  },
+  {
+    "revision": "42ba3d241752489317b8",
+    "url": "js/chunk-vendors.6533d65f.js"
+  },
+  {
+    "revision": "576255d425b4b27cc64764b896d506cb",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "87d3392219ef931e2dad2b75b242a69c",
+    "url": "mstile-150x150.png"
+  },
+  {
+    "revision": "2ad4442bab02bd70d0294df0338a882a",
+    "url": "safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "site.webmanifest"
+  }
+]);
